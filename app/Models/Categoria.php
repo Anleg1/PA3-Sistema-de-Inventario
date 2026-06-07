14717f0f6d3c3f60b39242ba113cf030def43f6f
Anleg1 <60947271@mail.isil.pe>
2026-06-07 01:11:18 -0500

    Agregados controladores API con index y store

---
+++ app/Models/Categoria.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+  use HasFactory;
+
+    protected $table = 'productos';
+
+    protected $fillable = [
+        'nombre',
+        'precio',
+        'stock'
+    ];
+}
